c5358176873078b589366ea7a6d86f1ee0ab82f2
NewMicRob <microbnew@gmail.com>
2025-04-22 17:22:46 -0400

    created getTotalValue

--- week-13/script.js
+++ week-13/script.js
@@ -5,4 +5,8 @@ class productProperties { //name of the class being ccreated
         this.price = price; //price of a product
         this.quantity = quantity; //quantity of a product
     }
+
+    getTotalValue() { //method to return the calculated total value of a product
+        return this.price * this.quantity; //returns the total value of a product multiplies price times quantity
+    }
 }
