0ba474e15150a198a26ec3016654709defd37b6b
NewMicRob <microbnew@gmail.com>
2025-04-22 17:36:55 -0400

    removed = in sublass, moved subclass out of parent added toString to child

--- week-13/script.js
+++ week-13/script.js
@@ -13,11 +13,14 @@ class productProperties { //name of the class being ccreated
     toString() { //method to return the string representation of the product
         return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Total Value: ${this.getTotalValue()}`; //returns the string of the products name its price and quantity
     }
+}
 
-    perishableProductProperties = class extends productProperties { //class that extends the productProperties class
-        constructor(name, price, quantity, expirationDate) { //constructor of the class
-            super(name, price, quantity); //calls the parent class
-            this.expirationDate = expirationDate; //adds expiratoion date attribute/property
-        }
+class perishableProductProperties extends productProperties { //class that extends the productProperties class
+    constructor(name, price, quantity, expirationDate) { //constructor of the class
+        super(name, price, quantity); //calls the parent class
+        this.expirationDate = expirationDate; //adds expiratoion date attribute/property
+    }
+    toString() { //method to return the string representation of the perishable product
+        return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}, Total Value: ${this.getTotalValue()}`; //returns the string of the products name its price, quantity and expiration and total
     }
 }
