15460df0c7dc605f2adcff5a8d6366955dac0696
NewMicRob <microbnew@gmail.com>
2025-04-22 17:55:06 -0400

    created 2 instances for perishable, 2 instances for product,   logged instances for perishable  and fixed comment

--- week-13/script.js
+++ week-13/script.js
@@ -1,26 +1,45 @@
 //Part 1
-class productProperties { //name of the class being ccreated
-    constructor(name, price, quantity) { //class attributes
-        this.name = name; //name of a product
-        this.price = price; //price of a product
-        this.quantity = quantity; //quantity of a product
+class productProperties { //Mame of the class being created
+    constructor(name, price, quantity) { //Class attributes
+        this.name = name; //Name of a product
+        this.price = price; //Price of a product
+        this.quantity = quantity; //Quantity of a product
     }
 
-    getTotalValue() { //method to return the calculated total value of a product
-        return this.price * this.quantity; //returns the total value of a product multiplies price times quantity
+    getTotalValue() { //Method to return the calculated total value of a product
+        return this.price * this.quantity; //Returns the total value of a product multiplies price times quantity
     }
 
-    toString() { //method to return the string representation of the product
-        return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Total Value: ${this.getTotalValue()}`; //returns the string of the products name its price and quantity
+    toString() { //Method to return the string representation of the product
+        return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Total Value: ${this.getTotalValue()}`; //Returns the string of the products name its price and quantity
     }
 }
 
-class perishableProductProperties extends productProperties { //class that extends the productProperties class
-    constructor(name, price, quantity, expirationDate) { //constructor of the class
-        super(name, price, quantity); //calls the parent class
-        this.expirationDate = expirationDate; //adds expiratoion date attribute/property
+//Part 2
+class perishableProductProperties extends productProperties { //Class that extends the productProperties class
+    constructor(name, price, quantity, expirationDate) { //Constructor of the class
+        super(name, price, quantity); //Calls the parent class
+        this.expirationDate = expirationDate; //Adds expiratoion date attribute/property
     }
-    toString() { //method to return the string representation of the perishable product
-        return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}, Total Value: ${this.getTotalValue()}`; //returns the string of the products name its price, quantity and expiration and total
+
+    toString() { //Method to return the string representation of the perishable product
+        return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}, Total Value: ${this.getTotalValue()}`; //Returns the string of the products name its price, quantity and expiration and total
     }
 }
+//ProdcutProperties instances
+//let product1 = new productProperties('Apple', 1.25, 7);
+//let product2 = new productProperties('Kraft Dinner', 1, 4);
+
+//PerishableProductProperties instances
+let perishable1 = new perishableProductProperties('Milk', 1.5, 10, '2024-12-31');
+let perishable2 = new perishableProductProperties('Eggs', 2.0, 20, '2025-01-08');
+
+//Parent class log
+//console.log(product1.toString());
+//console.log(product2.toString());
+
+//Child class log
+console.log(perishable1.toString())
+console.log(perishable2.toString());
+
+//Part 3
