5afd349b984a889846e65ddc5169f4546c7dd86e
NewMicRob <microbnew@gmail.com>
2025-04-22 17:28:33 -0400

    added perishableProductProperties

--- week-13/script.js
+++ week-13/script.js
@@ -13,4 +13,11 @@ class productProperties { //name of the class being ccreated
     toString() { //method to return the string representation of the product
         return `Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Total Value: ${this.getTotalValue()}`; //returns the string of the products name its price and quantity
     }
+
+    perishableProductProperties = class extends productProperties { //class that extends the productProperties class
+        constructor(name, price, quantity, expirationDate) { //constructor of the class
+            super(name, price, quantity); //calls the parent class
+            this.expirationDate = expirationDate; //adds expiratoion date attribute/property
+        }
+    }
 }
